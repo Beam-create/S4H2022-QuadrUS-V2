254665ec314a5554e456359b34e62e9f2df76b75
Chrispako990210 <72101538+Chrispako990210@users.noreply.github.com>
2022-03-10 00:20:59 -0500

    Correction, mkdir nested folder
    Pour mkdir, rajouter l'option -p pour creer des nested folders.

--- Installation.sh
+++ Installation.sh
@@ -11,10 +11,10 @@ echo "--> Installation de ROS noetic ..."
 echo ""
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl || FAIL_ROS=true
+sudo apt-get install curl -y || FAIL_ROS=true
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt update || FAIL_ROS=true
-sudo apt install ros-noetic-desktop-full || FAIL_ROS=true
+sudo apt-get update || FAIL_ROS=true
+sudo apt-get install ros-noetic-desktop-full -y || FAIL_ROS=true
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
@@ -39,8 +39,8 @@ fi
 echo ""
 echo "--> Installation de modules python ..."
 echo ""
-sudo apt install python3-pip || FAIL_PYTHON=true
-sudo apt install python-is-python3 || FAIL_PYTHON=true
+sudo apt-get install python3-pip -y || FAIL_PYTHON=true
+sudo apt-get install python-is-python3 -y || FAIL_PYTHON=true
 python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose || FAIL_PYTHON=true
 
 if $FAIL_PYTHON; then
@@ -52,11 +52,11 @@ echo ""
 echo "--> Création des dossiers du projet"
 echo ""
 
-mkdir ~/rufus_ws/src
-cd ~/rufus_ws/src || return
+mkdir -p ~/rufus_ws/src
+cd ~/rufus_ws/src || exit 1
 catkin_init_workspace || FAIL_REPO=true
-git clone "https://github.com/Beam-create/S4H2022-RufUS.git" || FAIL_REPO=true
-cd ~/rufus_ws || return
+git clone "https://github.com/Beam-create/S4H2022-RufUS.git" || exit 1
+cd ~/rufus_ws || exit 1
 catkin_make || FAIL_REPO=true
 echo "source ~/rufus_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
